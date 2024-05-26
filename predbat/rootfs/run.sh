754384ce3ae1e4a18dfd66d7e3c7485dee958801
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-05-26 18:07:28 +0100

    Update run.sh

--- predbat/rootfs/run.sh
+++ predbat/rootfs/run.sh
@@ -1,3 +1,4 @@
-echo "Running App Process Management"
+echo "Running Predbat inside Add-on"
 echo "Your API key is: $SUPERVISOR_TOKEN"
+cp /hass.py /config/hass.py
 python3 /startup.py $SUPERVISOR_TOKEN
