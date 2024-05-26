f8f295e3da4ab19a58181ab69b2ee479311b32aa
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-05-26 18:14:01 +0100

    Update hass.py

--- predbat/rootfs/hass.py
+++ predbat/rootfs/hass.py
@@ -43,7 +43,7 @@ async def main():
     py_files = []
     for root, dirs, files in os.walk("."):
         for file in files:
-            if file.endswith(".py"):
+            if file.endswith(".py") or file.endswith(".yaml"):
                 py_files.append(os.path.join(root, file))
     print("Watching {} for changes".format(py_files))
 
