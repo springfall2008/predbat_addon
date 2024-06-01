daa51622ff97169f09b18c0083c80f9afc5894ef
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-06-01 11:49:48 +0100

    Update run.sh

--- predbat/rootfs/run.sh
+++ predbat/rootfs/run.sh
@@ -1,4 +1,8 @@
 echo "Running Predbat inside Add-on"
 echo "Your API key is: $SUPERVISOR_TOKEN"
-cp /hass.py /config/hass.py
+if [ -f /config/dev ]; then
+    echo "Dev mode enabled, not copying hass.py"
+else
+    cp /hass.py /config/hass.py
+fi
 python3 /startup.py $SUPERVISOR_TOKEN
