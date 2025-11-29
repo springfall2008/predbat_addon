f411792e38a7ab966fba22c10baa80f266f032a2
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2025-11-29 19:44:18 +0000

    Removing hass from add on (#80)
    * Fix conditional logic for copying hass.py
    
    * Bump version from 1.3.3 to 1.4.0

--- predbat/rootfs/run.sh
+++ predbat/rootfs/run.sh
@@ -1,7 +1,7 @@
 echo "Running Predbat inside Add-on"
 echo "Your API key is: $SUPERVISOR_TOKEN"
-if [ -f /config/dev ]; then
-    echo "Dev mode enabled, not copying hass.py"
+if [ -f /config/hass.py ]; then
+    echo "Hass already installed not copying it"
 else
     cp /hass.py /config/hass.py
 fi
