de6a901da55015d1e72cb404c47f9683f5cecec2
nipar4 <nic.parkin@tpf2003.co.uk>
2025-12-22 15:33:05 +0000

    First pass at s6 overlay

---
+++ predbat/rootfs/alpine/run.docker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+echo "[predbat] Docker startup"
+
+# Prepare config
+if [ -z "$(ls -A /config)" ]; then
+    echo "/config directory is empty, copying files"
+    cp -v /addon/apps.yaml /config/
+else
+    if [ ! -f /config/apps.yaml ]; then
+        cp -v /addon/apps.yaml /config/
+    fi
+fi
+
+# Block until config is valid
+while grep -q "^[^#]*template: True" /config/apps.yaml; do
+    echo "#################################################"
+    echo "Please update apps.yaml"
+    echo "Remove 'template: True'"
+    echo "#################################################"
+    sleep 30
+done
+
+echo "[predbat] Starting Predbat"
+
+# 🔑 THIS LINE IS CRITICAL
+exec python3 /addon/startup.py
